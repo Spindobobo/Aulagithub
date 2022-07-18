4f079437469ed16f470ad407b1e070f437dd1dcd
Rafael Spindola <rafa.spin95@gmail.com>
2022-07-17 21:02:01 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+        <div class="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/spindobobo/">@spindobobo</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
